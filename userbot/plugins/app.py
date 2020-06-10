cfa8f935a7764d586f6aa695c84ca0544c3b3b9c
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-06-10 21:01:09 +0430

    Update app.py

--- userbot/plugins/app.py
+++ userbot/plugins/app.py
@@ -41,8 +41,8 @@ async def apk(e):
         app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
         app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
         app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
-        app_details += "\n\n===> @FridayOT <==="
-        await e.edit(app_details, link_preview = True, parse_mode = 'HTML')
+        app_details += "\n\n===> CipherX Exclusive <==="
+
     except IndexError:
         await e.edit("No result found in search. Please enter **Valid app name**")
     except Exception as err:
