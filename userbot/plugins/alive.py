e0003d52a38d0ea712006b5f81f164446d04c846
SHER321 <66766275+SHER321@users.noreply.github.com>
2020-06-16 19:56:55 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,7 +13,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**MY STATUS** \n`FRIDAY IS:` **✅ Alive**\n\n"
+    await alive.edit("**MY STATUS** \n`NGYUSER IS:` **✅ Alive**\n\n"
                      "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
                      "`DATABASE STATUS:` **ALL NORMAL! WORKING FINE 🙂**\n`NO PROBLEM FOUND🔥!\n`"
                      "`CONNECTION PROVIDER:` ** AMAZON WEB SERVICES\n"
@@ -22,10 +22,10 @@ async def amireallyalive(alive):
                      "`IP:` 3.82.66.232\n"
                      "`SYSTEM TYPE:` LINUX\n"
                      "`BASED ON:` UNIBORG\n"
-                     "`SATELLITE:` ⚡STARK SAT-2⚡\n"
+                     "`SATELLITE:` 😎 N I K H I L 😎\n"
                      "`MADE USING:` PYTHON\n"
-                     f"`MY BOSS`: {DEFAULTUSER}\n\n"
+                     f"`MY BOSS`: @NGYNY \n\n"
                      "ALWAYS WITH YOU MY BOSS\n\n"
                      "`SATELLITE STATUS: ✅ Alive\n\n"
-                     "[⚡DEPLOY FRIDAY⚡](https://github.com/StarkGang/FRIDAYUserbot)") 
+                    
 
