702dbe9805b1b4af581ffa17858a45fa9331ac7c
SHER321 <66766275+SHER321@users.noreply.github.com>
2020-06-16 18:57:19 +0530

    Update animedp.py

--- userbot/plugins/animedp.py
+++ userbot/plugins/animedp.py
@@ -1,4 +1,4 @@
-#Made By @MarioDevs Keep Credits If You Are Goanna Kang This Lol
+#Made By @NGYNY Keep Credits If You Are Goanna Kang This Lol
 
 #And Thanks To The Creator Of Autopic This Script Was Made from Snippets From That Script
 
@@ -62,7 +62,7 @@ async def animepp():
 
 async def main(event):
 
-    await event.edit("**Starting Anime Profile Pic...\n\nDone !!! Check Your DP By @MarioDevs**")
+    await event.edit("**Starting Anime Profile Pic...\n\nDone !!! Check Your DP By @NGYNY**")
 
     while True:
 
