6af1f2569bb462ca0580188d1b1081b2dda18976
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-06-21 14:33:15 +0430

    update remove.bg.py

--- userbot/plugins/remove.bg.py
+++ userbot/plugins/remove.bg.py
@@ -47,7 +47,7 @@ async def _(event):
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
         with io.BytesIO(output_file_name.content) as remove_bg_image:
-            remove_bg_image.name = "@FridayOT.png"
+            remove_bg_image.name = "CipherX.png"
             await borg.send_file(
                 event.chat_id,
                 remove_bg_image,
@@ -58,9 +58,9 @@ async def _(event):
             )
         end = datetime.now()
         ms = (end - start).seconds
-        await event.edit("Removed dat annoying Backgroup in {} seconds, powered by @IndianArMyGiveaway".format(ms))
+        await event.edit("Removed dat annoying Backgroup in {} seconds, powered by CipherX".format(ms))
     else:
-        await event.edit("ReMove.BG API returned Errors. Please report to @IndianArMyGiveaway\n`{}".format(output_file_name.content.decode("UTF-8")))
+        await event.edit("ReMove.BG API returned Errors. Please report to CipherX\n`{}".format(output_file_name.content.decode("UTF-8")))
 
 
 # this method will call the API, and return in the appropriate format
