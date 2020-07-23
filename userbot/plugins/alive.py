17ad0d0f99d1de6e975b9731ea598557ae1c452c
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-07-23 13:42:19 +0430

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,31 +1,27 @@
 """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+#IMG CREDITS: @Hackintush
 import asyncio
 from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
+from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
-from userbot.utils import admin_cmd
-
-
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-
+from telethon.tl.types import ChannelParticipantsAdmins
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
+PM_IMG = "https://filetolinktelegrambot.herokuapp.com/1996057911608/VID_20200723_004420.mp4"
+pm_caption = "`CɪᴘʜᴇʀX Super Technology Bot is:` **Online**\n\n"
+pm_caption += "**System Status**\n"
+pm_caption += "`Telethon Version:` **6.0.9**\n`Python:` **3.7.4**\n"
+pm_caption += "`Database Status:` **Working Properly**\n"
+pm_caption += "**Current Branch** : `Master`\n"
+pm_caption += "**CɪᴘʜᴇʀX OS** : `3.14`\n"
+pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
+pm_caption += "**Database** : `AWS - Working Properly`\n\n"
+pm_caption += "**CɪᴘʜᴇʀX is the best**" 
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
+    chat = await alive.get_chat()
+    await alive.delete()
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**MY STATUS** \n`CipherX IS:` **✅ Alive**\n\n"
-                     "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
-                     "`DATABASE STATUS:` **ALL NORMAL! WORKING FINE 😄**\n`NO PROBLEM FOUND!\n`"
-                     "`CONNECTION PROVIDER:` ** AMAZON WEB SERVICES\n"
-                     "`CURRECT BOT LOCATION:` ** ASHBURN,UNITED STATES\n"
-                     "`DEPLOYING SERVICE BY: HEROKU INC.\n"
-                     "`IP:` Just CipherX knows that😁\n"
-                     "`SYSTEM TYPE:` LINUX\n"
-                     "`BASED ON:` TELETHON\n"
-                     "`SATELLITE:` ⚡STARK SAT-2⚡\n"
-                     "`MADE USING:` PYTHON\n"
-                     f"`MY BOSS`: {DEFAULTUSER}\n\n"
-                     "ALWAYS WITH YOU MY BOSS\n\n"
-                     "`SATELLITE STATUS: ✅ Alive\n\n")
-
-
+    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await alive.delete() 
+    
