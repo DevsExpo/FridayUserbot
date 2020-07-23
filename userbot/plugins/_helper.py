056c57dac3d666a7efa6a13c21dd7b256c83b72b
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-07-23 12:59:09 +0000

    update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -39,7 +39,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Master of Robots\n [CipherX](https://t.me/Hackintush)\n`Help menu to reveal all modules`"""
+            help_string = """Master of Robots\n[✨CipherX✨](https://t.me/Hackintush)\n`This is the help menu for modules`"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
