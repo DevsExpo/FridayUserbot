582b842236bf0a7307d07de7bf977c571c2d9334
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-07-28 09:13:02 +0000

    update proxyscrapecipherx.py

--- userbot/plugins/proxyscrapecipherx.py
+++ userbot/plugins/proxyscrapecipherx.py
@@ -6,11 +6,11 @@ from userbot.utils import admin_cmd
 from pySmartDL import SmartDL
 import os
 
-CɪᴘʜᴇʀX_HTTP = "https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=3000&country=IR&ssl=all&anonymity=all" 
+CɪᴘʜᴇʀX_Http = "https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=3000&country=IR&ssl=all&anonymity=all" 
 HTTP_TXT = ("**Proxy Info** \nType: __Https__ \nTimeOut: __3000__ \nCountry: __IR__ \nSsl: All \nAnonymity: __All__ \nUploaded By [CɪᴘʜᴇʀX](https://t.me/Hackintush) \n**Here Is Your Proxy** 👇")
-CɪᴘʜᴇʀX_SOCKS4 = "https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=3000&country=IR"
+CɪᴘʜᴇʀX_Socks4 = "https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=3000&country=IR"
 SOCKS4_TXT = ("**Proxy Info** \nType: __Socks5__ \nTimeOut: __10000__ \nCountry: __All__ \nSsl: __Only For Http Proxy__ \nAnonymity: __Only For Http__ \nUploaded By [CɪᴘʜᴇʀX](https://t.me/Hackintush) \n**Here Is Your Proxy** 👇")
-CɪᴘʜᴇʀX_SOCKS5 = "https://api.proxyscrape.com/?request=getproxies&proxytype=socks5&timeout=3000&country=IR"
+CɪᴘʜᴇʀX_Socks5 = "https://api.proxyscrape.com/?request=getproxies&proxytype=socks5&timeout=3000&country=IR"
 SOCKS5_TXT = ("**Proxy Info** \nType: __Socks5__ \nTimeOut: __10000__ \nCountry: __All__ \nSsl: __Only For Http Proxy__ \nAnonymity: __Only For Http__ \nUploaded By [CɪᴘʜᴇʀX](https://t.me/Hackintush) \n**Here Is Your Proxy** 👇")
 
 @borg.on(admin_cmd(pattern="http")) 
@@ -18,7 +18,7 @@ async def CɪᴘʜᴇʀXhttp(event):
     chat = await event.get_chat() 
     file_name = "Http_CɪᴘʜᴇʀX.txt"
     downloaded_file_name = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY , file_name)
-    downloader = SmartDL( f"{CipherX_Http}" , downloaded_file_name, progress_bar=False)
+    downloader = SmartDL( f"{CɪᴘʜᴇʀX_Http}" , downloaded_file_name, progress_bar=False)
     downloader.start(blocking=False) 
     await borg.send_message(event.chat_id , HTTP_TXT)
     await event.client.send_file(event.chat_id , downloaded_file_name) 
@@ -28,7 +28,7 @@ async def CɪᴘʜᴇʀXsocks4(event):
     chat = await event.get_chat() 
     file_name = "Socks4_CɪᴘʜᴇʀX.txt"
     downloaded_file_name = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY , file_name)
-    downloader = SmartDL( f"{CipherX_Socks4}" , downloaded_file_name, progress_bar=False)
+    downloader = SmartDL( f"{CɪᴘʜᴇʀX_Socks4}" , downloaded_file_name, progress_bar=False)
     downloader.start(blocking=False) 
     await borg.send_message(event.chat_id , SOCKS4_TXT)
     await event.client.send_file(event.chat_id , downloaded_file_name) 
@@ -38,7 +38,7 @@ async def CɪᴘʜᴇʀXsocks5(event):
     chat = await event.get_chat() 
     file_name = "Socks5_CɪᴘʜᴇʀX.txt"
     downloaded_file_name = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY , file_name)
-    downloader = SmartDL( f"{CipherX_Socks5}" , downloaded_file_name, progress_bar=False)
+    downloader = SmartDL( f"{CɪᴘʜᴇʀX_Socks5}" , downloaded_file_name, progress_bar=False)
     downloader.start(blocking=False) 
     await borg.send_message(event.chat_id , SOCKS5_TXT)
     await event.client.send_file(event.chat_id , downloaded_file_name) 
