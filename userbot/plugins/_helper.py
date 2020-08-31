46b0a9e47147e6c44125e9d5c32bedb48fc5b676
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-09-01 01:38:30 +0430

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -31,11 +31,9 @@ async def cmd_list(event):
             help_string = """`Help Menu To Show Modules Of Super Technology Bot Made By`\n[CɪᴘʜᴇʀX](t.me/Hackintush) \n__Do .help plugin_name for commands, in case popup doesn't appear.__"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
-                help_string
-            )
+                help_string)
             await results[0].click(
                 event.chat_id,
                 reply_to=event.reply_to_msg_id,
-                hide_via=True
-            )
+                hide_via=True)
             await event.delete()
