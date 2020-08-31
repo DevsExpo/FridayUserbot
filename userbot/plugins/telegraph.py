2be235a008cc0fb3dff48be4a09abee731b1369e
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-08-31 22:56:54 +0430

    Update telegraph.py

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -18,13 +18,13 @@ auth_url = r["auth_url"]
 async def _(event):
     if event.fwd_from:
         return
-    if Config.PLUGIN_CHANNEL is None:
-        await event.edit("Please set the required environment variable `PLUGIN_CHANNEL` for this plugin to work")
+    if Config.PRIVATE_GROUP_BOT_API_ID is None:
+        await event.edit("Please set the required environment variable `PRIVATE_GROUP_BOT_API_ID` for this plugin to work")
         return
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     await borg.send_message(
-        Config.PLUGIN_CHANNEL,
+        Config.PRIVATE_GROUP_BOT_API_ID,
         "Created New Telegraph account {} for the current session. \n**Do not give this url to anyone, even if they say they are from Telegram!**".format(auth_url)
     )
     optional_title = event.pattern_match.group(2)
@@ -82,7 +82,7 @@ async def _(event):
             ms = (end - start).seconds
             await event.edit("Pasted to https://telegra.ph/{} in {} seconds.".format(response["path"], ms), link_preview=True)
     else:
-        await event.edit("Reply to a message to get a permanent telegra.ph link. (Inspired by CɪᴘʜᴇʀX)")
+        await event.edit("Reply to a message to get a permanent telegra.ph link. (Inspired by @ControllerBot)")
 
 
 def resize_image(image):
