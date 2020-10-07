332fc965f4371b3ce0869e003d9e2b9ac2f23bbd
Restyled.io <commits@restyled.io>
2020-10-07 16:41:35 +0000

    Restyled by autopep8

--- userbot/plugins/assistant/start.py
+++ userbot/plugins/assistant/start.py
@@ -1,4 +1,4 @@
-#    Copyright (C) Midhun KM 
+#    Copyright (C) Midhun KM
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -42,40 +42,41 @@ async def start(event):
     starttext = ("Hi! this is An Assistant Bot For My [Owner] ")
     if event.from_id == bot.uid:
         await tgbot.send_message(
-           vent,
-           message="Hi Master, It's Me Your Assistant.",
-           buttons = [
-           [custom.Button.inline("Broadcast 🔥", data="mebroadcast")],
-           [Button.url("Repo?", "https://github.com/StarkGang/FridayUserbot")],
-           [Button.url("Join Channel 📃", "t.me/Fridayot")]
+            vent,
+            message="Hi Master, It's Me Your Assistant.",
+            buttons=[
+                [custom.Button.inline("Broadcast 🔥", data="mebroadcast")],
+                [Button.url("Repo?", "https://github.com/StarkGang/FridayUserbot")],
+                [Button.url("Join Channel 📃", "t.me/Fridayot")]
             ]
-           )
+        )
     else:
         await tgbot.send_message(
-           event.chat_id,
-           message=starttext,
-           link_preview=False,
-           buttons = [
-           [custom.Button.inline("Deploy your Friday 🇮🇳", data="deploy")],
-           [Button.url("Help Me ❓", "t.me/Fridayot")]
-       ]
-      )
+            event.chat_id,
+            message=starttext,
+            link_preview=False,
+            buttons=[
+                [custom.Button.inline("Deploy your Friday 🇮🇳", data="deploy")],
+                [Button.url("Help Me ❓", "t.me/Fridayot")]
+            ]
+        )
 
 
 # Data's
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"deploy")))
 async def help(event):
-        await event.delete()
-        if event.query.user_id is not bot.uid:
-            await tgbot.send_message(
-                event.chat_id,
-                message="You Can Deploy Friday In Heroku By Following Steps Bellow, You Can See Some Quick Guides On Support Channel Or On Your Own Assistant Bot. \nThank You For Contacting Me.",
-                buttons = [
-                [Button.url("Deploy Tutorial 📺", "https://youtu.be/xfHcm_e92eQ")],
+    await event.delete()
+    if event.query.user_id is not bot.uid:
+        await tgbot.send_message(
+            event.chat_id,
+            message="You Can Deploy Friday In Heroku By Following Steps Bellow, You Can See Some Quick Guides On Support Channel Or On Your Own Assistant Bot. \nThank You For Contacting Me.",
+            buttons=[
+                [Button.url("Deploy Tutorial 📺",
+                            "https://youtu.be/xfHcm_e92eQ")],
                 [Button.url("Need Help ❓", "t.me/FridaySupportOfficial")]
-                 ]
-                )
+            ]
+        )
 
 # Bot Permit.
 @tgbot.on(events.NewMessage(func=lambda e: e.is_private))
@@ -90,7 +91,7 @@ async def all_messages_catcher(event):
         sender = await event.get_sender()
         chat_id = event.chat_id
         sed = await event.forward_to(bot.uid)
-        
+
 
 # Add User To Database ,Later For Broadcast Purpose
 # (C) @SpecHide
@@ -101,11 +102,10 @@ async def all_messages_catcher(event):
         )
 
 
-
-# Test 
+# Test
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"mebroadcast")))
 @tgbot.on(events.NewMessage)
 async def sed(event):
-        replied = event.raw_text
-        tgbot.send_message(event.chat_id, replied)
-        await tgbot.send_message("Ok, Broadcasting This Message")
+    replied = event.raw_text
+    tgbot.send_message(event.chat_id, replied)
+    await tgbot.send_message("Ok, Broadcasting This Message")
