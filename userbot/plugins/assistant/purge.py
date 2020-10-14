58e3f6d473bb68f140d2f5378521e3c0899cb82e
Restyled.io <commits@restyled.io>
2020-10-14 06:04:45 +0000

    Restyled by autopep8

--- userbot/plugins/assistant/purge.py
+++ userbot/plugins/assistant/purge.py
@@ -2,7 +2,7 @@
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
-# 
+#
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -10,8 +10,6 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 
 
 from telethon import events, custom, Button
@@ -67,11 +65,13 @@ from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.utils import admin_cmd, errors_handler, register, sudo_cmd
 OWNER_ID = bot.uid
 # Check if user has admin rights
+
+
 async def is_administrator(user_id: int, message):
     admin = False
     async for user in tgbot.iter_participants(message.chat_id,
-                             filter=ChannelParticipantsAdmins):
-        if user_id == user.id or OWNER_ID :
+                                              filter=ChannelParticipantsAdmins):
+        if user_id == user.id or OWNER_ID:
             admin = True
             break
     return admin
@@ -79,44 +79,44 @@ async def is_administrator(user_id: int, message):
 
 @tgbot.on(events.NewMessage(pattern='^/purge'))
 async def purge(event):
-        chat = event.chat_id
-        msgs = []
+    chat = event.chat_id
+    msgs = []
 
-        if not await is_administrator(user_id=event.from_id, message=event):
-           await event.reply("You're not an admin!")
-           return
+    if not await is_administrator(user_id=event.from_id, message=event):
+        await event.reply("You're not an admin!")
+        return
 
-        msg = await event.get_reply_message()
-        if not msg:
-           await event.reply("Reply to a message to select where to start purging from.")
-           return
+    msg = await event.get_reply_message()
+    if not msg:
+        await event.reply("Reply to a message to select where to start purging from.")
+        return
 
-        try:
-           msg_id = msg.id
-           count = 0        
-           to_delete = event.message.id - 1
-           await tgbot.delete_messages(chat, event.message.id)
-           msgs.append(event.reply_to_msg_id)
-           for m_id in range(to_delete, msg_id - 1, -1):
-               msgs.append(m_id)
-               count += 1            
-               if len(msgs) == 100:
-                   await tgbot.delete_messages(chat, msgs)
-                   msgs = []
+    try:
+        msg_id = msg.id
+        count = 0
+        to_delete = event.message.id - 1
+        await tgbot.delete_messages(chat, event.message.id)
+        msgs.append(event.reply_to_msg_id)
+        for m_id in range(to_delete, msg_id - 1, -1):
+            msgs.append(m_id)
+            count += 1
+            if len(msgs) == 100:
+                await tgbot.delete_messages(chat, msgs)
+                msgs = []
 
-           await tgbot.delete_messages(chat, msgs)
-           del_res = await tgbot.send_message(
-           event.chat_id, f"Fast Purged {count} messages.")
+        await tgbot.delete_messages(chat, msgs)
+        del_res = await tgbot.send_message(
+            event.chat_id, f"Fast Purged {count} messages.")
 
-           await asyncio.sleep(4)
-           await del_res.delete()
+        await asyncio.sleep(4)
+        await del_res.delete()
 
-        except MessageDeleteForbiddenError:
-            text = "Failed to delete messages.\n"
-            text += "Messages maybe too old or I'm not admin! or dont have delete rights!"
-            del_res = await respond(text, parse_mode='md')
-            await asyncio.sleep(5)
-            await del_res.delete()
+    except MessageDeleteForbiddenError:
+        text = "Failed to delete messages.\n"
+        text += "Messages maybe too old or I'm not admin! or dont have delete rights!"
+        del_res = await respond(text, parse_mode='md')
+        await asyncio.sleep(5)
+        await del_res.delete()
 
 
 @tgbot.on(events.NewMessage(pattern="^/del$"))
