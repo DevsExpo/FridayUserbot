a3c15428cda09ee03a88af3dd1965b55ee336370
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-10-22 16:06:15 +0330

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,6 +1,34 @@
 from telethon import events
 from datetime import datetime
+import time
+from userbot import Lastupdate, bot
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
 
 @command(pattern="^.ping")
 async def _(event):
@@ -10,4 +38,5 @@ async def _(event):
     await event.edit("P I N G")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit("P I N G\n{}".format(ms))
+    uptime = get_readable_time((time.time() - Lastupdate))
+    await event.edit("P I N G\n{}".format(ms)\n ➲ `{uptime}`)
