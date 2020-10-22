aedb49e2cb7e75225da125a5d5861a13ebe48eac
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-10-22 15:46:23 +0330

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,13 +7,13 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://filetolinktelegrambot.herokuapp.com/1996057911608/VID_20200723_004420.mp4"
-pm_caption = "`CɪᴘʜᴇʀX Super Technology Bot is:` **Online**\n\n"
-pm_caption += "**System Status**\n"
-pm_caption += "`Telethon Version:` **6.0.9**\n`Python:` **3.8.5**\n"
-pm_caption += "`Database Status:` **AWS Working Properly**\n"
-pm_caption += "**Current Branch** : `Master`\n"
-pm_caption += "**CɪᴘʜᴇʀX OS** : `Kali GNU/Linux Rolling x86_64`\n"
-pm_caption += f"**My Boss** : [CɪᴘʜᴇʀX](https://t.me/Hackintush)\n"
+pm_caption = "`➥ CɪᴘʜᴇʀX Super Technology Bot is:` **Online**\n\n"
+pm_caption += "**➥ System Status**\n"
+pm_caption += "`➥ Telethon Version:` **6.0.9**\n`Python:` **3.8.5**\n"
+pm_caption += "`➥ Database Status:` **AWS Working Properly**\n"
+pm_caption += "**➥ Current Branch** : `Master`\n"
+pm_caption += "**➥ CɪᴘʜᴇʀX OS** : `Kali GNU/Linux Rolling x86_64`\n"
+pm_caption += f"**➥ My Boss** : [CɪᴘʜᴇʀX](https://t.me/Hackintush)\n"
 pm_caption += "**✨ CɪᴘʜᴇʀX is the best ✨**" 
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
