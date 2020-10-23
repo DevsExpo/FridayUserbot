ef093e0250fd847f7fb79e073a5a2d71c20e0fd2
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-10-23 16:25:00 +0330

    Update bye.py

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -1,33 +1,24 @@
 # For @UniBorg
+# Courtesy @Hackintush
 
-"""use cmd `.leave` and kick yourself from the group chat"""
-
-
+"""
+.bye
+"""
+import time
 
 from telethon.tl.functions.channels import LeaveChannelRequest
 
-from uniborg.util import admin_cmd
-
-import time
+from userbot.utils import friday_on_cmd, edit_or_reply, sudo_cmd
 
 
-
-
-
-@borg.on(admin_cmd("bye", outgoing=True))
-
+@friday.on(friday_on_cmd("bye", outgoing=True))
+@friday.on(sudo_cmd("bye", allow_sudo=True))
 async def leave(e):
-
+    starkgang = await edit_or_reply(e, "Bye")
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-
-        await e.edit("`I am leaving this chat.....!`")
-
+        await starkgang.edit("`I am leaving this chat.....!`")
         time.sleep(3)
-
-        if '-' in str(e.chat_id):
-
+        if "-" in str(e.chat_id):
             await borg(LeaveChannelRequest(e.chat_id))
-
         else:
-
-            await e.edit('`Sir This is Not A Chat`')
+            await starkgang.edit("`Sir This is Not A Chat`")
