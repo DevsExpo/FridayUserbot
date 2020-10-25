22a53a5b8e845075fb133db284131a6240384fdd
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-10-26 02:25:37 +0330

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -94,7 +94,9 @@ async def carbon_api(e):
         await e.client.send_file(
             e.chat_id,
             file,
-            caption="<< Here's your carbon \n Carbonised by [CipherX](https://t.me/Hackintush)>>,
+            caption="<< `Here's your carbon!` \n **Carbonised Using** [CɪᴘʜᴇʀX](https://t.me/Hackintush)>>\n**Colour Scheme: **`{}`".format(
+                color_name
+            ), 
             force_document=True,
             reply_to=e.message.reply_to_msg_id,
         )
