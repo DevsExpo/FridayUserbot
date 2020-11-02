01dc27f41cb72aeea0efb9ffe6729076b16cd3d9
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-02 18:55:07 +0330

    Update howtogoogle.py

--- userbot/plugins/howtogoogle.py
+++ userbot/plugins/howtogoogle.py
@@ -1,25 +1,27 @@
-#Modded from dagd.py
+# Modded from dagd.py
 """
 Animate How To Google
 Command .ggl Search Query
-By @loxxi
+By @Hackintush
 """
 
-from telethon import events
-import os
 import requests
-import json
-from userbot.utils import admin_cmd
+
+from userbot.utils import friday_on_cmd
 
 
-@borg.on(admin_cmd("ggl (.*)"))
+@friday.on(friday_on_cmd("ggl (.*)"))
 async def _(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    sample_url = "https://da.gd/s?url=https://lmgtfy.com/?q={}%26iie=1".format(input_str.replace(" ","+"))
+    sample_url = "https://da.gd/s?url=https://lmgtfy.com/?q={}%26iie=1".format(
+        input_str.replace(" ", "+")
+    )
     response_api = requests.get(sample_url).text
     if response_api:
-        await event.edit("[{}]({})\n`Thank me Later 🙃` ".format(input_str,response_api.rstrip()))
+        await event.edit(
+            "[{}]({})\n`Thank's to [CɪᴘʜᴇʀX](https://t.me/Hackintush) 🙃` ".format(input_str, response_api.rstrip())
+        )
     else:
         await event.edit("something is wrong. please try again later.")
