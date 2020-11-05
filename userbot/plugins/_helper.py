b0a1d3d3ae7300c4219a1b26b6810e171c06f958
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-05 20:28:49 +0330

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -29,7 +29,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """ CipherX Bot Modules Are Listed Here !\n
+            help_string = """Modules Are Listed Here !\n
 Programmed and Managed by CɪᴘʜᴇʀX"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
