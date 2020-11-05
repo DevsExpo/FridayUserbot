1f3c9cffef51cb70dd52b8fc947ce8afd7414991
CipherX <cipherxx1998@gmail.com>
2020-11-05 12:05:26 +0000

    pylint: auto fixes

--- userbot/plugins/memify.py
+++ userbot/plugins/memify.py
@@ -30,9 +30,7 @@ async def _(event):
         await event.edit("```Reply to actual users message.```")
         return
     else:
-        await event.edit(
-            "```Transfiguration Time! Memifying this image! (」ﾟﾛﾟ)｣ ```"
-        )
+        await event.edit("```Transfiguration Time! Memifying this image! (」ﾟﾛﾟ)｣ ```")
     await borg.download_file(reply_message.media)
 
     async with borg.conversation("@MemeCreatorBot") as bot_conv:
@@ -102,9 +100,7 @@ async def _(event):
             await asyncio.sleep(4)
             sax.delete()
         elif not is_message_image(reply_message):
-            await event.edit(
-                "Invalid message type. Please choose right message type."
-            )
+            await event.edit("Invalid message type. Please choose right message type.")
             return
         else:
             await borg.send_file(event.chat_id, response.media)
