297505450ec8e0a2bc0bd64a16c07c0d7ec1d71a
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-05 15:52:23 +0330

    Update mashup.py

--- userbot/plugins/mashup.py
+++ userbot/plugins/mashup.py
@@ -1,40 +1,13 @@
 # created by @Hackintush
 """
 video meme mashup:
-Syntax: .mashup <text>
+Syntax: .mash <text>
 """
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from uniborg.util import friday_on_cmd
 
 from userbot import CMD_HELP
-
-
-@friday.on(friday_on_cmd(pattern="mashup ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    reply_to_id = event.message
-    if event.reply_to_msg_id:
-        reply_to_id = await event.get_reply_message()
-    chat = "@vixtbot"
-    await event.edit("```Checking...```")
-    async with event.client.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=285336877)
-            )
-            await event.client.send_message(chat, "{}".format(input_str))
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("Unblock @vixtbot")
-            return
-        if response.text.startswith("I can't find that"):
-            await event.edit("sorry I can't find it")
-        else:
-            await event.delete()
-            await borg.send_file(event.chat_id, response.message, reply_to=reply_to_id)
 
 
 @friday.on(friday_on_cmd(pattern="mash ?(.*)", allow_sudo=True))
@@ -46,7 +19,8 @@ async def _(event):
     if event.reply_to_msg_id:
         reply_to_id = await event.get_reply_message()
     chat = "@vixtbot"
-    await event.delete()
+    await event.edit("```Checking...```")
+    await alive.delete()
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -55,10 +29,10 @@ async def _(event):
             await event.client.send_message(chat, "{}".format(input_str))
             response = await response
         except YouBlockedUserError:
-            await event.reply("Unblock @vixtbot")
+            await event.reply("Unblock bot")
             return
         if response.text.startswith("I can't find that"):
-            await event.reply("sorry I can't find it")
+            await event.edit("Sorry, I can't find it")
         else:
             await event.delete()
             await borg.send_file(event.chat_id, response.message, reply_to=reply_to_id)
@@ -66,7 +40,7 @@ async def _(event):
 
 CMD_HELP.update(
     {
-        "mashup": "`.mashup` <text> :\
+        "mashup": "`.mash` <text> :\
       \n**USAGE:** Send you the related video message of given text . "
     }
 )
