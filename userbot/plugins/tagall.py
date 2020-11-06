d99682a02124864e7cadb0b47a3f957ec3340d69
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-07 00:13:17 +0330

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -32,7 +32,7 @@ async def _(event):
         return
     mentions = "All members tagged successfully. Honor CɪᴘʜᴇʀX!"
     chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, PeerUser):
+    async for x in borg.iter_participants(chat, 200):
         mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
     await event.reply(mentions)
     await event.delete()
