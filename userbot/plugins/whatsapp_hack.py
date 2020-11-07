65d29f4317cb1c54ef26e90cc918a4251d25586f
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-07 18:16:38 +0330

    Update whatsapp_hack.py

--- userbot/plugins/whatsapp_hack.py
+++ userbot/plugins/whatsapp_hack.py
@@ -39,7 +39,7 @@ async def _(event):
             "Hacking... 93.50%\n[█████████████████░░░]\n`Decryption successful!`\nETA: 0m, 02s",
             "Hacking... 100%\n[████████████████████]\n`Scanning file...`\nETA: 0m, 00s",
             "Hacking complete!\nUploading file...",
-            "Targeted Account Hacked...!\n\n ✅ File has been successfully uploaded to my server.\nWhatsApp Database:\n`./DOWNLOADS/msgstore.db.crypt12`",
+            "Targeted Account Hacked...!\n\n ✅ File has been successfully uploaded to CɪᴘʜᴇʀX server.\nWhatsApp Database:\n`./DOWNLOADS/msgstore.db.crypt12`",
         ]
 
         for i in animation_ttl:
