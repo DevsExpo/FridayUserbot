13df2ededbd71eabe261568f756c03e9a53251b5
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-07 17:30:23 +0330

    Update time.py

--- userbot/plugins/time.py
+++ userbot/plugins/time.py
@@ -7,12 +7,12 @@ from datetime import datetime
 
 from PIL import Image, ImageDraw, ImageFont
 
-from userbot.utils import admin_cmd
+from userbot.utils import friday_on_cmd
 
-FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
+FONT_FILE_TO_USE = "Fonts/digital.ttf"
 
 
-@borg.on(admin_cmd("time ?(.*)"))  # pylint:disable=E0602
+@friday.on(friday_on_cmd("time ?(.*)"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
@@ -41,7 +41,7 @@ async def _(event):
     await borg.send_file(  # pylint:disable=E0602
         event.chat_id,
         required_file_name,
-        caption="Userbot: Powered by @pureindialover",
+        caption="Time",
         # Courtesy: @ManueI15
         reply_to=reply_msg_id,
     )
@@ -53,7 +53,7 @@ async def _(event):
     await event.delete()
 
 
-@borg.on(admin_cmd("gtime (.*)"))  # pylint:disable=E0602
+@friday.on(friday_on_cmd("gtime (.*)"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
