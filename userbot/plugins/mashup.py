db69de1d895f3af431a6690737d7fb708dfe3579
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-08 17:39:51 +0330

    Update mashup.py

--- userbot/plugins/mashup.py
+++ userbot/plugins/mashup.py
@@ -1,7 +1,7 @@
-# created by @Hackintush
+# created by @Mr_Hops
 """
 video meme mashup:
-Syntax: .vix <text>
+Syntax: .mashup <text>
 """
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -10,7 +10,34 @@ from uniborg.util import friday_on_cmd
 from userbot import CMD_HELP
 
 
-@friday.on(friday_on_cmd(pattern="vix ?(.*)", allow_sudo=True))
+@friday.on(friday_on_cmd(pattern="mashup ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    reply_to_id = event.message
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    chat = "@vixtbot"
+    await event.edit("```Checking...```")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=285336877)
+            )
+            await event.client.send_message(chat, "{}".format(input_str))
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("Unblock @vixtbot")
+            return
+        if response.text.startswith("I can't find that"):
+            await event.edit("sorry i can't find it")
+        else:
+            await event.delete()
+            await borg.send_file(event.chat_id, response.message, reply_to=reply_to_id)
+
+
+@friday.on(friday_on_cmd(pattern="mash ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -39,7 +66,7 @@ async def _(event):
 
 CMD_HELP.update(
     {
-        "mashup": "`.vix` <text> :\
+        "mashup": "`.mashup` <text> :\
       \n**USAGE:** Send you the related video message of given text . "
     }
 )
