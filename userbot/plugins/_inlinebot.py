7f25b6c231fb747c9149ee99f80b3652b41bd558
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-11 18:47:19 +0330

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -139,10 +139,15 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"dontspamnigga")))
     async def rip(event):
         await event.get_chat()
+        him_id = event.query.user_id
         text1 = "You Have Chosed A Probhited Option. Therefore, You Have Been Blocked. "
         await event.edit("Choice Not Accepted ❌")
         await borg.send_message(event.query.user_id, text1)
         await borg(functions.contacts.BlockRequest(event.query.user_id))
+        await tgbot.send_message(
+            LOG_CHAT,
+            f"Hello, [This](tg://user?id={him_id}) Selected Probhited Option, Therefore Blocked.",
+        )
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"whattalk")))
     async def rip(event):
@@ -160,6 +165,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"askme")))
     async def rip(event):
         await event.get_chat()
+        him_id = event.query.user_id
         await event.edit("Choice Accepted ✔️")
         text3 = "Ok, Wait. You can Ask After Master Approves You. Kindly, Wait."
         await borg.send_message(event.query.user_id, text3)
