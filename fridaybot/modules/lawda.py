7ea71685e6ed8325cb11c50d29c729070b2d4d16
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2020-11-12 12:56:56 +0530

    Add files via upload

---
+++ fridaybot/modules/lawda.py
@@ -0,0 +1,54 @@
+#Credit To @Kraken_The_BadASS . Keep credit if you are going to edit it.
+
+
+import random, re
+from uniborg.util import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="lawda ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("A")
+        await asyncio.sleep(0.2)
+        await event.edit("Ab")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba l")
+        await asyncio.sleep(0.2)
+        await event.edit("aba la")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba law")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawd")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda b")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be f")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be fa")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be far")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be far f")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be far fr")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be far fro")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be far from")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be far from p")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be far from pr")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be far from pro")
+        await asyncio.sleep(0.2)
+        await event.edit("Aba lawda be far from pros")
+        await asyncio.sleep(0.2)
+        await event.edit("**Aba lawda be far from pros**😈")
