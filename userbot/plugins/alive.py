3630b53ad1a19c8f85e852cf970ea68ef18120bf
CipherX <cipherxx1998@gmail.com>
2020-11-22 17:22:07 +0000

    pylint: auto fixes

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -2,10 +2,7 @@
 # IMG CREDITS: @Hackintush
 from uniborg.util import friday_on_cmd, sudo_cmd
 
-from userbot import ALIVE_NAME, CMD_HELP, Lastupdate
-from userbot.Configs import Config
-from userbot.modules import currentversion
-
+from userbot import ALIVE_NAME, CMD_HELP
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = (
