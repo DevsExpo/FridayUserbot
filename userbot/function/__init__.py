24a3373c9b11d38bacf14880c64d9c1051be0487
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-22 17:44:02 +0330

    Update __init__.py

--- userbot/function/__init__.py
+++ userbot/function/__init__.py
@@ -1,8 +1,1 @@
-# Api(s)
-jiobase = "https://www.jiosaavn.com/api.php?__call=autocomplete.get&_format=json&_marker=0&cc=in&includeMetaTags=1&query="
-# https://github.com/Cyberboysumanjay/apis
-jiocbs = "http://starkmusic.herokuapp.com/result/?query="
-torrents = "https://api.sumanjay.cf/torrent/?query="
-webshot = "https://api.sumanjay.cf/ss/?url="
-covid_global = "https://api.sumanjay.cf/covid/?country="
-ekart = "https://api.sumanjay.cf/ekart/"
+
