5495cc18362c6e272deb65f48a3a338bb0cef6d7
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-22 18:14:06 +0330

    Create imagetools.py

---
+++ userbot/plugins/imagetools.py
@@ -0,0 +1,123 @@
+# Api Provided By @PythonvsAno
+#    Copyright (C) Midhun KM 2020
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+
+import requests
+from telethon.tl.types import MessageMediaPhoto
+
+from userbot.utils import friday_on_cmd, sudo_cmd
+
+sedpath = "./starkgangz/"
+if not os.path.isdir(sedpath):
+    os.makedirs(sedpath)
+
+
+@friday.on(friday_on_cmd(pattern=r"cit"))
+@friday.on(sudo_cmd(pattern=r"cit", allow_sudo=True))
+async def hmm(event):
+    life = Config.DEEP_API_KEY
+    if life == None:
+        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+        await event.edit("No Api Key Found, Please Add it. For Now Using Local Key")
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    headers = {"api-key": life}
+    hmm = await event.edit("Colorizing..")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in response.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    img_file = {
+        "image": open(img, "rb"),
+    }
+    url = "https://api.deepai.org/api/colorizer"
+    r = requests.post(url=url, files=img_file, headers=headers).json()
+    sedimg = r["output_url"]
+    await borg.send_file(event.chat_id, sedimg)
+    await hmm.delete()
+    if os.path.exists(img):
+        os.remove(img)
+
+
+@friday.on(friday_on_cmd(pattern=r"toon"))
+@friday.on(sudo_cmd(pattern=r"toon", allow_sudo=True))
+async def hmm(event):
+    life = Config.DEEP_API_KEY
+    if life == None:
+        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+        await event.edit("No Api Key Found, Please Add it. For Now Using Local Key")
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    headers = {"api-key": life}
+    hmm = await event.edit("Tuning...")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in response.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    img_file = {
+        "image": open(img, "rb"),
+    }
+    url = "https://api.deepai.org/api/toonify"
+    r = requests.post(url=url, files=img_file, headers=headers).json()
+    sedimg = r["output_url"]
+    await borg.send_file(event.chat_id, sedimg)
+    await hmm.delete()
+    if os.path.exists(img):
+        os.remove(img)
+
+
+@friday.on(friday_on_cmd(pattern=r"nst"))
+@friday.on(sudo_cmd(pattern=r"nst", allow_sudo=True))
+async def hmm(event):
+    life = Config.DEEP_API_KEY
+    if life == None:
+        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+        await event.edit("No Api Key Found, Please Add it. For Now Using Local Key")
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    headers = {"api-key": life}
+    hmm = await event.edit("Colourzing..")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in response.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    img_file = {
+        "image": open(img, "rb"),
+    }
+    url = "https://api.deepai.org/api/nsfw-detector"
+    r = requests.post(url=url, files=img_file, headers=headers).json()
+    sedcopy = r["output"]
+    hmmyes = sedcopy["detections"]
+    game = sedcopy["nsfw_score"]
+    final = f"**IMG RESULT** \n**Detections :** `{hmmyes}` \n**NSFW SCORE :** `{game}`"
+    await borg.send_message(event.chat_id, final)
+    await hmm.delete()
+    if os.path.exists(img):
+        os.remove(img)
