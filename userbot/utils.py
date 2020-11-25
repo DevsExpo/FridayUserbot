8b317d2252aabfbe3015af23be3e1e3380ab5c3f
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-11-25 15:54:40 +0330

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -11,6 +11,7 @@ from userbot.Configs import Config
 from userbot.wraptools import am_i_admin, ignore_bot, ignore_fwd, ignore_grp, ignore_pm
 from var import Var
 
+sedprint = logging.getLogger("PLUGINS")
 cmdhandler = Config.COMMAND_HAND_LER
 bothandler = Config.BOT_HANDLER
 
@@ -99,7 +100,7 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        print("Successfully (re)imported " + shortname)
+        sedprint.info("Successfully (re)imported " + shortname)
     else:
         import importlib
         import sys
@@ -135,7 +136,7 @@ def load_module(shortname):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["userbot.plugins." + shortname] = mod
-        print("Successfully imported " + shortname)
+        sedprint.info("Successfully imported " + shortname)
 
 
 def remove_plugin(shortname):
@@ -650,8 +651,8 @@ def start_assistant(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        print("Starting Your Assistant Bot.")
-        print("Assistant Sucessfully imported " + shortname)
+        sedprint.info("Starting Your Assistant Bot.")
+        sedprint.info("Assistant Sucessfully imported " + shortname)
     else:
         import importlib
         import sys
@@ -675,4 +676,4 @@ def start_assistant(shortname):
         mod.only_pvt = only_pvt()
         spec.loader.exec_module(mod)
         sys.modules["userbot.plugins.assistant" + shortname] = mod
-        print("Assistant Has imported " + shortname)
+        sedprint.info("Assistant Has imported " + shortname)
