63d0863d63079f0a4ac3ea042f2cc2838393e9b2
CipherX <cipherxx1998@gmail.com>
2020-11-26 18:42:04 +0000

    pylint: auto fixes

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -107,7 +107,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 )
 async def on_plug_in_callback_query_handler(event):
     if not event.query.user_id == bot.uid:
-        sedok ="Don't touch that button bitch!"
+        sedok = "Don't touch that button bitch!"
         await event.answer(sedok, cache_time=0, alert=True)
         return
     plugin_name = event.data_match.group(1).decode("UTF-8")
