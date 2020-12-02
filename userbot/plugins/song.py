af7639e06248f149cef3595011101f3ecc7d1261
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-02 20:17:07 +0330

    Update song.py

--- userbot/plugins/song.py
+++ userbot/plugins/song.py
@@ -91,9 +91,9 @@ CMD_HELP.update(
     {
         "song": "**Song**\
         \n\n**Syntax : **`.spd <song name>`\
-        \n**Usage :** downloads the song from Spotify and uploades it.
+        \n**Usage :** downloads the song from Spotify and uploades it.\
         \n\n**Syntax : **`.netease <song name>`\
-        \n**Usage :** downloads the song from internet and uploades it.
+        \n**Usage :** downloads the song from internet and uploades it.\
         \n\n**Syntax : **`.dzd <song name>`\
         \n**Usage :** downloads the song from Deezer and uploades it."
     }
