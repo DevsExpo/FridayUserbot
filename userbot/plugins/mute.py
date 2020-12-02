9a1bc72b381cacd57a6927cb3610d3a3fc1538b9
CipherX <cipherxx1998@gmail.com>
2020-12-02 13:12:26 +0000

    pylint: auto fixes

--- userbot/plugins/mute.py
+++ userbot/plugins/mute.py
@@ -14,8 +14,9 @@
 
 import asyncio
 
-from userbot.plugins.sql_helper.mute_sql import is_muted, mute, unmute
 from userbot import CMD_HELP
+from userbot.plugins.sql_helper.mute_sql import is_muted, mute, unmute
+
 
 @command(outgoing=True, pattern=r"^.mute ?(\d+)?")
 async def startmute(event):
@@ -217,7 +218,8 @@ async def hehehe(event):
                 chat,
                 "`This inbox has been blessed by my master. Consider yourself lucky.`\n**Increased Stability and Karma** (づ￣ ³￣)づ",
             )
-            
+
+
 CMD_HELP.update(
     {
         "mute": "**Mute**\
