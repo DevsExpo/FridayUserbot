f1f4c64d178c3b92f0b9d2c42b9b492b8ab547c0
CipherX <cipherxx1998@gmail.com>
2020-12-02 11:41:51 +0000

    pylint: auto fixes

--- userbot/plugins/cow.py
+++ userbot/plugins/cow.py
@@ -5,7 +5,9 @@ and may cmd would be added soon."""
 
 from cowpy import cow
 from telethon import events
+
 from userbot import CMD_HELP
+
 
 @friday.on(events.NewMessage(pattern=r"^.(\w+)say (.*)", outgoing=True))
 async def univsaye(cowmsg):
@@ -22,7 +24,8 @@ async def univsaye(cowmsg):
         cheese = cheese()
 
         await cowmsg.edit(f"`{cheese.milk(text).replace('`', '´')}`")
-                          
+
+
 CMD_HELP.update(
     {
         "cow": "**Cow**\
