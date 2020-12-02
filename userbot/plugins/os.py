e404a6431ea12449f05036b0f021536f331c0cbf
CipherX <cipherxx1998@gmail.com>
2020-12-02 13:18:31 +0000

    pylint: auto fixes

--- userbot/plugins/os.py
+++ userbot/plugins/os.py
@@ -13,7 +13,9 @@ Available Commands:
 import asyncio
 
 from telethon import events
+
 from userbot import CMD_HELP
+
 
 @friday.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
@@ -199,7 +201,8 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 7])
-            
+
+
 CMD_HELP.update(
     {
         "os": "**OS**\
