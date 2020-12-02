e30c5773a79c539b132485be99c34eaa9d7a31cc
CipherX <cipherxx1998@gmail.com>
2020-12-02 12:37:16 +0000

    pylint: auto fixes

--- userbot/plugins/jainder.py
+++ userbot/plugins/jainder.py
@@ -4,7 +4,9 @@
 import random
 
 from telethon import events
+
 from userbot import CMD_HELP
+
 
 @friday.on(events.NewMessage(pattern=r"\.jainder(.*)", outgoing=True))
 async def _(event):
@@ -103,7 +105,8 @@ async def _(event):
     index = random.randint(0, len(emoticons))
     output_str = emoticons[index]
     await event.edit(output_str)
-    
+
+
 CMD_HELP.update(
     {
         "jainder": "**Jainder**\
