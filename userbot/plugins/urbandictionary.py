fb32c1de8f452c57eaf7bda22f8d95c53a09f2e8
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-02 18:50:11 +0330

    Update urbandictionary.py

--- userbot/plugins/urbandictionary.py
+++ userbot/plugins/urbandictionary.py
@@ -6,7 +6,7 @@ Syntax: .ud Query"""
 import urbandict
 
 from userbot.utils import friday_on_cmd
-
+from userbot import CMD_HELP
 
 @friday.on(friday_on_cmd("ud (.*)"))
 async def _(event):
@@ -31,3 +31,11 @@ async def _(event):
             await event.edit("No result found for **" + str + "**")
     except:
         await event.edit("No result found for **" + str + "**")
+        
+CMD_HELP.update(
+    {
+        "urbandictionary": "**Urban Dictionary**\
+\n\n**Syntax : **`.ud <word>`\
+\n**Usage :** Gives meaning of the word from urban dictionary."
+    }
+)
