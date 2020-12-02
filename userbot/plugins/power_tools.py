0e92e97f7e8ed336c3435825ddf995b00268a238
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-02 17:20:13 +0330

    Update power_tools.py

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -9,18 +9,18 @@ import os
 import sys
 
 from userbot.utils import friday_on_cmd
-
+from userbot import CMD_HELP
 
 @friday.on(friday_on_cmd("restart"))
 async def _(event):
     if event.fwd_from:
         return
-    # await asyncio.sleep(2)
-    # await event.edit("Restarting [██░] ...\n`.ping` me or `.help` to check if I am online after a lil bit.")
-    # await asyncio.sleep(2)
-    # await event.edit("Restarting [███]...\n`.ping` me or `.help` to check if I am online after a lil bit.")
-    # await asyncio.sleep(2)
-    await event.edit("Restarted. `.ping` me or `.helpme` to check if I am online")
+    await asyncio.sleep(2)
+    await event.edit("Restarting [██░] ...\n`.ping` me or `.help` to check if I'm online after a lil bit.")
+    await asyncio.sleep(2)
+    await event.edit("Restarting [███]...\n`.ping` me or `.help` to check if I'm online after a lil bit.")
+    await asyncio.sleep(2)
+    await event.edit("Restarted. `.ping` me or `.help` me to check if I'm online")
     await borg.disconnect()
     # https://archive.is/im3rt
     os.execl(sys.executable, sys.executable, *sys.argv)
@@ -34,3 +34,13 @@ async def _(event):
         return
     await event.edit("Turning off ...Manually turn me on later")
     await borg.disconnect()
+    
+CMD_HELP.update(
+    {
+        "power_tools": "**Power Tools**\
+\n\n**Syntax : **`.restart`\
+\n**Usage :** restarts CɪᴘʜᴇʀX bot.\
+\n\n**Syntax : **`.shutdown`\
+\n**Usage :** Shuts down your CɪᴘʜᴇʀX bot."
+    }
+)
