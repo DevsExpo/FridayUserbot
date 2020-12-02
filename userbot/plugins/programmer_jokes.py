f3d85a0360fe5559b5ee45b713ae6e90cc1bc485
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-02 17:22:18 +0330

    Create programmer_jokes.py

---
+++ userbot/plugins/programmer_jokes.py
@@ -0,0 +1,20 @@
+import pyjokes
+from uniborg.util import friday_on_cmd
+
+from userbot import CMD_HELP
+
+
+@friday.on(friday_on_cmd(pattern=r"pjoke"))
+async def hi(event):
+    if event.fwd_from:
+        return
+    await event.edit(pyjokes.get_joke(category="all"))
+
+
+CMD_HELP.update(
+    {
+        "programmer_jokes": "**Programmer Jokes**\
+\n\n**Syntax : **`.pjoke`\
+\n**Usage :** Get programmer jokes."
+    }
+)
