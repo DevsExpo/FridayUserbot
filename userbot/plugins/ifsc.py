cfb82770b4b9bd9c4eb0366095c99b9ba6555162
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-06 16:18:04 +0330

    Update ifsc.py

--- userbot/plugins/ifsc.py
+++ userbot/plugins/ifsc.py
@@ -1,69 +1,47 @@
-"""iffuci.tk pastebin site
-Code written by @Hackintush {iffuci}
-Syntax: .iffuci"""
-import os
-from datetime import datetime
+#    Copyright (C) @Hackintush
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import requests
+from uniborg.util import friday_on_cmd
 
 from userbot import CMD_HELP
-from userbot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
 
 
-def progress(current, total):
-    logger.info(
-        "Downloaded {} of {}\nCompleted {}".format(
-            current, total, (current / total) * 100
-        )
-    )
-
-
-@friday.on(friday_on_cmd(pattern="iffuci ?(.*)"))
-@friday.on(sudo_cmd(pattern="iffuci ?(.*)", allow_sudo=True))
+@friday.on(friday_on_cmd(pattern="ifsc (.*)"))
 async def _(event):
-    crackexy = await edit_or_reply(event, "Processing")
     if event.fwd_from:
         return
-    start = datetime.now()
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     input_str = event.pattern_match.group(1)
-    message = "SYNTAX: `.iffuci <long text to include>`"
-    if input_str:
-        message = input_str
-    elif event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        if previous_message.media:
-            downloaded_file_name = await borg.download_media(
-                previous_message,
-                Config.TMP_DOWNLOAD_DIRECTORY,
-                progress_callback=progress,
-            )
-            m_list = None
-            with open(downloaded_file_name, "rb") as fd:
-                m_list = fd.readlines()
-            message = ""
-            for m in m_list:
-                message += m.decode("UTF-8") + "\r\n"
-            os.remove(downloaded_file_name)
-        else:
-            message = previous_message.message
-    else:
-        message = "SYNTAX: `.iffuci <long text to include>`"
-    url = "https://www.iffuci.tk/documents"
-    r = requests.post(url, data=message.encode("UTF-8")).json()
-    url = f"https://iffuci.tk/{r['key']}"
-    end = datetime.now()
-    ms = (end - start).seconds
-    if r["isUrl"]:
-        nurl = f"https://iffuci.tk/v/{r['key']}"
-        await crackexy.edit(
-            "code is pasted to {} in {} seconds. GoTo Original URL: {}".format(
-                url, ms, nurl
-            )
-        )
-    else:
-        await crackexy.edit("code is pasted to {} in {} seconds".format(url, ms))
+
+    IFSC_Code = input_str
+
+    URL = "https://ifsc.razorpay.com/"
+
+    data = requests.get(URL + IFSC_Code).json()
+
+    a = data["ADDRESS"]
+    b = data["CENTRE"]
+    c = data["BRANCH"]
+    d = data["CITY"]
+    e = data["STATE"]
+    f = data["BANK"]
+    g = data["BANKCODE"]
+    h = data["IFSC"]
+
+    await event.edit(
+        f"<b><u>**Information Gathered Successfully**</b></u>\n\n<b>Bank Name :-</b><code>{f}</code>\n<b>Bank Address:- </b> <code>{a}</code>\n<b>Centre :-</b><code>{b}</code>\n<b>Branch :- </b><code>{c}</code>\n<b> City :-</b><code>{d}</code>\n<b>State:- </b> <code>{e}</code>\n<b>Bank Code :- </b><code>{g}</code>\n<b>Ifsc :-</b><code>{h}</code>",
+        parse_mode="HTML",
+    )
 
 
 CMD_HELP.update(
