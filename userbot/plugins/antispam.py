66bcd27e5a8e85d7bff5bd8e676f29b171585c48
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-09 02:57:40 +0330

    Update antispam.py

--- userbot/plugins/antispam.py
+++ userbot/plugins/antispam.py
@@ -1,6 +1,6 @@
 from telethon.events import ChatAction
 
-from userbot import sclient
+from userbot import bot, sclient
 from userbot.Configs import Config
 
 """Bans Spammers/Scammer At time Of Arrival 
@@ -9,14 +9,17 @@ If You Add Him The Bot Won't Restrict."""
 
 @borg.on(ChatAction)
 async def ok(event):
+    juser = await event.get_user()
     if Config.ANTISPAM_FEATURE != "ENABLE":
         return
     if event.user_joined:
-        juser = await event.get_user()
+        hmmyep = await borg.get_permissions(event.chat_id, bot.uid)
+        if not hmmyep.is_admin:
+            return
         user = sclient.is_banned(juser.id)
-        if user.banned == True:
+        if user:
             await event.reply(
-                f"**#CɪᴘʜᴇʀX Anti-Spam Algorithm** \n**Detected Malicious User.** \n**User-ID :** `{juser.id}`  \n**Reason :** `{user.reason}`"
+                f"**CɪᴘʜᴇʀX Anti-Spam Algorithm** \n**Detected Malicious User.** \n**User-ID :** `{juser.id}`  \n**Reason :** `{user.reason}`"
             )
             try:
                 await borg.edit_permissions(
