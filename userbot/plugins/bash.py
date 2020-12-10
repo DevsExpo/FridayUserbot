e0e2d7578cb567b456ad528e3437597af2560030
CipherX <cipherxx1998@gmail.com>
2020-12-10 15:46:39 +0000

    pylint: auto fixes

--- userbot/plugins/bash.py
+++ userbot/plugins/bash.py
@@ -1,12 +1,9 @@
-from telethon import events
-import subprocess
-from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
 import asyncio
 import io
 import time
 
 from userbot import CMD_HELP
-from userbot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
+from userbot.utils import friday_on_cmd, sudo_cmd
 
 
 @friday.on(friday_on_cmd(pattern="bash ?(.*)"))
@@ -14,13 +11,12 @@ from userbot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
 async def _(event):
     if event.fwd_from:
         return
-    DELAY_BETWEEN_EDITS = 0.3
     PROCESS_RUN_TIME = 100
     cmd = event.pattern_match.group(1)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    start_time = time.time() + PROCESS_RUN_TIME
+    time.time() + PROCESS_RUN_TIME
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
@@ -44,11 +40,10 @@ async def _(event):
                 force_document=True,
                 allow_cache=False,
                 caption=cmd,
-                reply_to=reply_to_id
+                reply_to=reply_to_id,
             )
             await event.delete()
     await event.edit(OUTPUT)
-
 
 
 CMD_HELP.update(
