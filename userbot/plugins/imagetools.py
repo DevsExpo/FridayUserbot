d8a2efca53e5222f0a43611a92cc38548c818e7e
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-10 19:49:10 +0330

    Update imagetools.py

--- userbot/plugins/imagetools.py
+++ userbot/plugins/imagetools.py
@@ -20,8 +20,8 @@ from PIL import Image
 from telegraph import upload_file
 from telethon.tl.types import MessageMediaPhoto
 
-from userbot import CMD_HELP
-from userbot.utils import friday_on_cmd, sudo_cmd
+from fridaybot import CMD_HELP
+from fridaybot.utils import friday_on_cmd, sudo_cmd
 
 sedpath = "./starkgangz/"
 if not os.path.isdir(sedpath):
@@ -34,7 +34,7 @@ async def hmm(event):
     if not event.reply_to_msg_id:
         await event.reply("Reply to any Image.")
         return
-    hmmu = await event.edit("Colourizing...")
+    hmmu = await event.edit("Colorizing...")
     sed = await event.get_reply_message()
     if isinstance(sed.media, MessageMediaPhoto):
         img = await borg.download_media(sed.media, sedpath)
@@ -77,38 +77,7 @@ async def hmm(event):
             os.remove(files)
 
 
-@friday.on(friday_on_cmd(pattern=r"toon"))
-@friday.on(sudo_cmd(pattern=r"toon", allow_sudo=True))
-async def hmm(event):
-    life = Config.DEEP_API_KEY
-    if life == None:
-        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
-        await event.edit("No Api Key Found, Please Add it. For Now Using Local Key")
-    if not event.reply_to_msg_id:
-        await event.reply("Reply to any Image.")
-        return
-    headers = {"api-key": life}
-    hmm = await event.edit("Tuning...")
-    sed = await event.get_reply_message()
-    if isinstance(sed.media, MessageMediaPhoto):
-        img = await borg.download_media(sed.media, sedpath)
-    elif "image" in sed.media.document.mime_type.split("/"):
-        img = await borg.download_media(sed.media, sedpath)
-    else:
-        await event.edit("Reply To Image")
-        return
-    img_file = {
-        "image": open(img, "rb"),
-    }
-    url = "https://api.deepai.org/api/toonify"
-    r = requests.post(url=url, files=img_file, headers=headers).json()
-    sedimg = r["output_url"]
-    await borg.send_file(event.chat_id, sedimg)
-    await hmm.delete()
-    if os.path.exists(img):
-        os.remove(img)
-
-
+# Firstly Released By @DELETEDUSER420
 @friday.on(friday_on_cmd(pattern=r"nst"))
 @friday.on(sudo_cmd(pattern=r"nst", allow_sudo=True))
 async def hmm(event):
@@ -120,7 +89,7 @@ async def hmm(event):
         await event.reply("Reply to any Image.")
         return
     headers = {"api-key": life}
-    hmm = await event.edit("Colourizing...")
+    hmm = await event.edit("Colorizing...")
     sed = await event.get_reply_message()
     if isinstance(sed.media, MessageMediaPhoto):
         img = await borg.download_media(sed.media, sedpath)
@@ -150,7 +119,7 @@ async def iamthug(event):
     if not event.reply_to_msg_id:
         await event.reply("Reply to any Image.")
         return
-    hmm = await event.edit("`Converting to Thug Image..`")
+    hmm = await event.edit("`Converting To thug Image...`")
     sed = await event.get_reply_message()
     if isinstance(sed.media, MessageMediaPhoto):
         img = await borg.download_media(sed.media, sedpath)
@@ -172,11 +141,48 @@ async def iamthug(event):
         mask = mask.resize((w, h), Image.ANTIALIAS)
         offset = (x, y)
         background.paste(mask, offset, mask=mask)
-    file_name = "fridaythug.png"
+    file_name = "thug.png"
     ok = sedpath + "/" + file_name
     background.save(ok, "PNG")
     await borg.send_file(event.chat_id, ok)
     await hmm.delete()
+    for files in (ok, img):
+        if files and os.path.exists(files):
+            os.remove(files)
+
+
+import os
+
+import cv2
+
+
+@friday.on(friday_on_cmd(pattern=r"tni"))
+@friday.on(sudo_cmd(pattern=r"tni", allow_sudo=True))
+async def toony(event):
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    hmmu = await event.edit("`Converting Toonized Image...`")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in sed.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    imagez = cv2.imread(img)
+    cartoon_image_style_2 = cv2.stylization(
+        imagez, sigma_s=60, sigma_r=0.5
+    )  ## Cartoonify process.
+    # Save it
+    file_name = "Tooned.png"
+    ok = sedpath + "/" + file_name
+    cv2.imwrite(ok, cartoon_image_style_2)
+    # Upload it
+    await borg.send_file(event.chat_id, ok)
+    await hmmu.delete()
+    # Remove all Files
     for files in (ok, img):
         if files and os.path.exists(files):
             os.remove(files)
@@ -201,9 +207,44 @@ async def lolmetrg(event):
     open("triggered.gif", "wb").write(r.content)
     lolbruh = "triggered.gif"
     await borg.send_file(
-        event.chat_id, lolbruh, caption="You got triggered...", reply_to=sed
+        event.chat_id, lolbruh, caption="You got triggered....", reply_to=sed
     )
     for files in (lolbruh, img):
+        if files and os.path.exists(files):
+            os.remove(files)
+
+
+@friday.on(friday_on_cmd(pattern=r"jail"))
+@friday.on(sudo_cmd(pattern=r"jail", allow_sudo=True))
+async def hmm(event):
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    hmmu = await event.edit("Sending him to jail...🚶")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in sed.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    mon = "./resources/jail/jail.png"
+    foreground = Image.open(mon).convert("RGBA")
+
+    background = Image.open(img).convert("RGB")
+    with Image.open(img) as img:
+        width, height = img.size
+    fg_resized = foreground.resize((width, height))
+    background.paste(fg_resized, box=(0, 0), mask=fg_resized)
+
+    background.save("./starkgangz/testing.png")
+
+    file_name = "testing.png"
+    ok = "./starkgangz/" + file_name
+    await borg.send_file(event.chat_id, ok)
+    await hmmu.delete()
+    for files in (ok, img):
         if files and os.path.exists(files):
             os.remove(files)
 
@@ -213,13 +254,13 @@ CMD_HELP.update(
         "imagetools": "**imagetools**\
         \n\n**Syntax : **`.cit`\
         \n**Usage :** colourizes the given picture\
-        \n\n**Syntax : **`.toon`\
-        \n**Usage :** makes toon of the given image\
         \n\n**Syntax : **`.nst`\
         \n**Usage :** removes colours from image\
         \n\n**Syntax : ** `.thug`\
         \n**Usage :** makes a thug life meme image\
         \n\n**Syntax : ** `.tig`\
-        \n**Usage :** Makes a triggered gif of the replied image"
+        \n**Usage :** Makes a triggered gif of the replied image\
+        \n\n**Syntax : ** `.jail`\
+        \n**Usage :** Makes a jail image of the replied image"
     }
 )
