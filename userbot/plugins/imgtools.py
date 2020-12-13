4bf71377ec73a1e4e94fcb12e794aa9f8d2182d9
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-13 04:00:33 +0330

    Update imgtools.py

--- userbot/plugins/imgtools.py
+++ userbot/plugins/imgtools.py
@@ -1,7 +1,4 @@
-# Made for DARK COBRA
-# Thanks to @THE_B_LACK_HAT
-# Fixed and Edited by @danish_00
-# Kang with credits else gay..
+# All Credits Belong to @hackintush 
 
 import os
 
@@ -9,13 +6,8 @@ import requests
 from telethon.tl.types import MessageMediaPhoto
 
 from userbot import ALIVE_NAME, CMD_HELP
-from userbot.utils import admin_cmd, friday_on_cmd, sudo_cmd
+from userbot.utils import friday_on_cmd, sudo_cmd
 
-DARKCOBRA = (
-    Config.DEEP_API_KEY
-    if Config.DEEP_API_KEY
-    else "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
-)
 
 sedpath = "./cipherx/"
 if not os.path.isdir(sedpath):
@@ -24,8 +16,12 @@ if not os.path.isdir(sedpath):
 KANG_KAREGA_MC = str(ALIVE_NAME) if ALIVE_NAME else "CɪᴘʜᴇʀX"
 
 
-@bot.on(admin_cmd(pattern="enc ?(.*)", outgoing=True))
+@friday.on(friday_on_cmd(pattern=r"enc"))
+@friday.on(sudo_cmd(pattern=r"enc", allow_sudo=True)) 
 async def _(event):
+    life = Config.DEEP_API_KEY
+    if life == None:
+        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
     reply = await event.get_reply_message()
     if not reply:
 
@@ -46,7 +42,7 @@ async def _(event):
                 "style": f"{hm}",
                 "content": open(media, "rb"),
             },
-            headers={"api-key": DARKCOBRA},
+            headers={"api-key": life},
         )
 
         os.remove(media)
@@ -69,7 +65,7 @@ async def _(event):
                 "style": "https://telegra.ph/file/aaaa686bd3acff51208d7.jpg",
                 "content": open(media, "rb"),
             },
-            headers={"api-key": DARKCOBRA},
+            headers={"api-key": life},
         )
 
         os.remove(media)
@@ -86,7 +82,7 @@ async def _(event):
 
 
 @friday.on(friday_on_cmd(pattern=r"reso"))
-@friday.on(sudo_cmd(pattern=r"toon", allow_sudo=True))
+@friday.on(sudo_cmd(pattern=r"reso", allow_sudo=True))
 async def hmm(event):
     life = Config.DEEP_API_KEY
     if life == None:
@@ -116,6 +112,37 @@ async def hmm(event):
     if os.path.exists(img):
         os.remove(img)
 
+@friday.on(friday_on_cmd(pattern=r"cele"))
+@friday.on(sudo_cmd(pattern=r"cele", allow_sudo=True))
+async def hmm(event):
+    life = Config.DEEP_API_KEY
+    if life == None:
+        life = "quickstart-QUdJIGlzIGNvbWluZy4uLi4K"
+        await event.edit("No Api Key Found, Please Add it. For Now Using Local Key")
+    if not event.reply_to_msg_id:
+        await event.reply("Reply to any Image.")
+        return
+    headers = {"api-key": life}
+    hmm = await event.edit("Recognizing the Celebrity...")
+    sed = await event.get_reply_message()
+    if isinstance(sed.media, MessageMediaPhoto):
+        img = await borg.download_media(sed.media, sedpath)
+    elif "image" in sed.media.document.mime_type.split("/"):
+        img = await borg.download_media(sed.media, sedpath)
+    else:
+        await event.edit("Reply To Image")
+        return
+    img_file = {
+        "image": open(img, "rb"),
+    }
+    url = "https://api.deepai.org/api/celebrity-recognition"
+    r = requests.post(url=url, files=img_file, headers=headers).json()
+    sedimg = r["output_url"]
+    await borg.send_file(event.chat_id, sedimg)
+    await hmm.delete()
+    if os.path.exists(img):
+        os.remove(img)
+
 
 CMD_HELP.update(
     {
@@ -124,4 +151,14 @@ CMD_HELP.update(
     }
 )
 
-# hehe
+CMD_HELP.update(
+    {
+        "imgtools": "**imgtools**\
+        \n\n**Syntax : **`.enc`\
+        \n**Usage :** Enhancerize any anime pic.\
+        \n\n**Syntax : **`.reso`\
+        \n**Usage :** Makes a super resolution quality image from your picture.\
+        \n\n**Syntax : ** `.cele`\
+        \n**Usage :** Recognizes the celebrity."
+    }
+) 
