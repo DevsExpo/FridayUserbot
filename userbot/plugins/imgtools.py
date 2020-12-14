16224dab4ed006189804c95f0ecc7bc780951e13
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-14 21:17:11 +0330

    Update imgtools.py

--- userbot/plugins/imgtools.py
+++ userbot/plugins/imgtools.py
@@ -5,7 +5,7 @@ import os
 import requests
 from telethon.tl.types import MessageMediaPhoto
 
-from userbot import CMD_HELP
+from userbot import CMD_HELP, ALIVE_NAME 
 from userbot.utils import admin_cmd, friday_on_cmd, sudo_cmd
 
 sedpath = "./cipherx/"
