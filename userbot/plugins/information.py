82de6ae6432116844cecd457d14bd65f1667e999
CipherX <cipherxx1998@gmail.com>
2020-12-14 12:41:13 +0000

    pylint: auto fixes

--- userbot/plugins/information.py
+++ userbot/plugins/information.py
@@ -8,7 +8,7 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 
 from userbot import CMD_HELP
-from userbot.utils import edit_or_reply, friday_on_cmd, sudo_cmd
+from userbot.utils import friday_on_cmd, sudo_cmd
 
 
 @friday.on(friday_on_cmd("info ?(.*)"))
@@ -20,24 +20,24 @@ async def _(event):
     if replied_user is None:
         await event.edit(str(error_i_a))
         return False
-    replied_user_profile_photos = await borg(GetUserPhotosRequest(
-        user_id=replied_user.user.id,
-        offset=42,
-        max_id=0,
-        limit=80
-    ))
+    replied_user_profile_photos = await borg(
+        GetUserPhotosRequest(
+            user_id=replied_user.user.id, offset=42, max_id=0, limit=80
+        )
+    )
     replied_user_profile_photos_count = "NaN"
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
-    except AttributeError as e:
+    except AttributeError:
         pass
     user_id = replied_user.user.id
     first_name = html.escape(replied_user.user.first_name)
     if first_name is not None:
         first_name = first_name.replace("\u2060", "")
     last_name = replied_user.user.last_name
-    last_name = last_name.replace(
-        "\u2060", "") if last_name else ("Last Name not found")
+    last_name = (
+        last_name.replace("\u2060", "") if last_name else ("Last Name not found")
+    )
     user_bio = replied_user.about
     if user_bio is not None:
         user_bio = html.escape(replied_user.about)
@@ -46,7 +46,7 @@ async def _(event):
         dc_id, location = get_input_location(replied_user.profile_photo)
     except Exception as e:
         dc_id = "`Need a Profile Picture to check **this**`"
-        location = str(e)
+        str(e)
     caption = """User Info Extracted By <b> CɪᴘʜᴇʀXbot</b>
 <b>⫸ User ID</b>: <code>{}</code>
 <b>⫸ Link To Profile</b>: <a href='tg://user?id={}'>Click Here.</a>
@@ -70,8 +70,8 @@ async def _(event):
         replied_user.user.restricted,
         replied_user.user.verified,
         replied_user.user.bot,
-        common_chats
-    ) # if command is replied to self it shows 0 common groups
+        common_chats,
+    )  # if command is replied to self it shows 0 common groups
     message_id_to_reply = event.message.reply_to_msg_id
     if not message_id_to_reply:
         message_id_to_reply = event.message.id
@@ -82,7 +82,7 @@ async def _(event):
         parse_mode="HTML",
         file=replied_user.profile_photo,
         force_document=False,
-        silent=True
+        silent=True,
     )
     await event.delete()
 
@@ -93,15 +93,14 @@ async def get_full_user(event):
         if previous_message.forward:
             replied_user = await event.client(
                 GetFullUserRequest(
-                    previous_message.forward.sender_id or previous_message.forward.channel_id
+                    previous_message.forward.sender_id
+                    or previous_message.forward.channel_id
                 )
             )
             return replied_user, None
         else:
             replied_user = await event.client(
-                GetFullUserRequest(
-                    previous_message.sender_id
-                )
+                GetFullUserRequest(previous_message.sender_id)
             )
             return replied_user, None
     else:
