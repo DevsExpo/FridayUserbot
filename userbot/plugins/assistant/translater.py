2397b38b53e7ac07ecbba709e47bed0444153fe8
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2020-12-27 19:30:43 +0330

    Update translater.py

--- userbot/plugins/assistant/translater.py
+++ userbot/plugins/assistant/translater.py
@@ -1,38 +1,56 @@
-# Copyright (C) Midhun KM
+#    Copyright (C) Midhun KM 2020
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
 #
-# Please Don't Kang Without Credits
-# A Plugin For Assistant Bot
-# x0x
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 import emoji
 from googletrans import Translator
-from telethon import events
+from google_trans_new import google_translator
+import requests
+from deep_translator import GoogleTranslator
+from googletrans import LANGUAGES
+from langdetect import detect
 
-
-@tgbot.on(events.NewMessage(pattern="^/tr ?(.*)"))
+@assistant_cmd("tr", is_args=True)
 async def _(event):
     input_str = event.pattern_match.group(1)
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         text = previous_message.message
-        lan = input_str or "gu"
+        lan = input_str or "en"
     elif "|" in input_str:
         lan, text = input_str.split("|")
     else:
         await tgbot.send_message(
-            event.chat_id, "`.tr LanguageCode` as reply to a message"
+            event.chat_id, "`/tr LanguageCode` as reply to a message"
         )
         return
     text = emoji.demojize(text.strip())
     lan = lan.strip()
-    translator = Translator()
-    translated = translator.translate(text, dest=lan)
-    after_tr_text = translated.text
-    output_str = (
-        f"**Translated By CipherX Assistant Bot** \n"
-        f"Source {translated.src} \nTranslation {lan} \nWhat I Can Translate From This {after_tr_text}"
-    )
+    translator = google_translator()
+    translated = translator.translate(text, lang_tgt=lan)
+    lmao_bruh = text
+    lmao = detect(text)
+    after_tr_text = lmao
+    source_lan = LANGUAGES[after_tr_text]
+    transl_lan = LANGUAGES[lan]
+    output_str = f"""**Translation by CɪᴘʜᴇʀX Super Technology Bot**
+**Source ({source_lan})**:
+`{text}`
+
+**Translation ({transl_lan})**:
+`{translated}`"""
+      
     try:
         await tgbot.send_message(event.chat_id, output_str)
     except Exception:
-        await tgbot.send_message(event.chat_id, "Something Went Wrong 🤔")
+        await tgbot.send_message(event.chat_id, "Something went wrong 🤔")
