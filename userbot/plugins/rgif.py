2a16ba2d29300f811ec4697ae242f5e5398399e1
CipherX <cipherxx1998@gmail.com>
2021-01-11 12:14:37 +0000

    pylint: auto fixes

--- userbot/plugins/rgif.py
+++ userbot/plugins/rgif.py
@@ -1,10 +1,17 @@
-from telethon import events
-import subprocess, os , asyncio, PIL, cv2, shutil
-from userbot.utils import admin_cmd 
+import asyncio
+import os
+import shutil
+
+import cv2
+import PIL
+
 from userbot import bot
+from userbot.utils import admin_cmd
+
 path = "./cipherx/"
 if not os.path.isdir(path):
     os.makedirs(path)
+
 
 @bot.on(admin_cmd(pattern="rgif"))
 async def _(event):
@@ -16,16 +23,22 @@ async def _(event):
     ret, frame = img.read()
     cv2.imwrite("cipher.png", frame)
     danish = PIL.Image.open("cipher.png")
-    dark,python = danish.size
-    cobra = f"""ffmpeg -f lavfi -i color=c=00ff00:s={dark}x{python}:d=10 -loop 1 -i cipher.png -filter_complex "[1]rotate=angle=PI*t:fillcolor=none:ow='hypot(iw,ih)':oh=ow[fg];[0][fg]overlay=x=(W-w)/2:y=(H-h)/2:shortest=1:format=auto,format=yuv420p" -movflags +faststart cipher.mp4 -y"""                 
+    dark, python = danish.size
+    cobra = f"""ffmpeg -f lavfi -i color=c=00ff00:s={dark}x{python}:d=10 -loop 1 -i cipher.png -filter_complex "[1]rotate=angle=PI*t:fillcolor=none:ow='hypot(iw,ih)':oh=ow[fg];[0][fg]overlay=x=(W-w)/2:y=(H-h)/2:shortest=1:format=auto,format=yuv420p" -movflags +faststart cipher.mp4 -y"""
     await event.edit("```Processing ...```")
     if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
+        event.reply_to_msg_id
     process = await asyncio.create_subprocess_shell(
-        cobra, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        cobra, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
     stdout, stderr = await process.communicate()
     await event.edit("```Uploading...```")
-    await event.client.send_file(event.chat_id, "cipher.mp4" , force_document=False, reply_to=event.reply_to_msg_id)
+    await event.client.send_file(
+        event.chat_id,
+        "cipher.mp4",
+        force_document=False,
+        reply_to=event.reply_to_msg_id,
+    )
     await event.delete()
     shutil.rmtree(path)
     os.remove("cipher.mp4")
