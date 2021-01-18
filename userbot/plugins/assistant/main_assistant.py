6e9d6d27f429ff0641344be6898fbc8d6749ba2c
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-01-18 11:10:56 +0330

    Update main_assistant.py

--- userbot/plugins/assistant/main_assistant.py
+++ userbot/plugins/assistant/main_assistant.py
@@ -16,8 +16,8 @@ import io
 import os
 import re
 
-import telethon
 from telethon import Button, custom, events, functions
+import telethon
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.utils import pack_bot_file_id
 
